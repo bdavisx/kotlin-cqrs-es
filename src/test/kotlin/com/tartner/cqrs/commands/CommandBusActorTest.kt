8739548111f6c1c6dd9ad5338a71aba3278bb1a5
bill <bdavisx@yahoo.com>
2018-06-22 22:12:25 -0500

    More CommandBus tests/refinements.

--- src/test/kotlin/com/tartner/cqrs/commands/CommandBusActorTest.kt
+++ src/test/kotlin/com/tartner/cqrs/commands/CommandBusActorTest.kt
@@ -1,16 +1,18 @@
 package com.tartner.cqrs.commands
 
-import com.natpryce.hamkrest.assertion.*
+import io.kotlintest.*
 import kotlinx.coroutines.experimental.*
 import kotlinx.coroutines.experimental.channels.*
 import org.junit.*
+import org.slf4j.*
 
 data class TestCommand(val id: Int): Command
 
 internal class CommandBusActorTest {
+  private val log = LoggerFactory.getLogger(CommandBusActorTest::class.java)
   @Test
   fun testMessageReceive() {
-    runBlocking {
+    runBlocking { withTimeout(500) {
       val commandChannel = Channel<Command>(Channel.UNLIMITED)
 
       val commandBus = CommandBusActor()
@@ -24,13 +26,14 @@ internal class CommandBusActorTest {
 
       commandBus.sendCommandToAddress(address, TestCommand(1))
       val receivedCommand =  receivedCommandDeferred.await()
-      println(receivedCommand)
-    }
+
+      receivedCommand shouldNotBe null
+    } }
   }
 
   @Test
   fun testNotBlocking() {
-    runBlocking {
+    runBlocking { withTimeout(500) {
       val commandChannel1 = Channel<Command>(1)
       val commandChannel2 = Channel<Command>(1)
 
@@ -48,8 +51,35 @@ internal class CommandBusActorTest {
       commandBus.sendCommandToAddress("2", TestCommand(3))
 
       val receivedCommand =  receivedCommandDeferred.await()
-      println(receivedCommand)
 
-    }
+      receivedCommand shouldNotBe null
+
+      log.debug(receivedCommand.toString())
+    } }
+  }
+
+  @Test
+  fun testMultipleCommands() {
+    runBlocking { withTimeout(500) {
+      val commandChannel1 = Channel<Command>(1)
+      val commandChannel2 = Channel<Command>(1)
+
+      val commandBus = CommandBusActor()
+
+      commandBus.registerChannelAtAddress("1", commandChannel1)
+      commandBus.registerChannelAtAddress("2", commandChannel2)
+
+      val launches = listOf<CompletableDeferred<Command>>(CompletableDeferred<Command>(),
+        CompletableDeferred<Command>(), CompletableDeferred<Command>())
+
+      launch { var count = 0; for (command in commandChannel1) launches[count++].complete(command) }
+      launch { for (command in commandChannel2) launches[2].complete(command) }
+
+      commandBus.sendCommandToAddress("1", TestCommand(1))
+      commandBus.sendCommandToAddress("1", TestCommand(2))
+      commandBus.sendCommandToAddress("2", TestCommand(3))
+
+      launches.awaitAll()
+    } }
   }
 }
